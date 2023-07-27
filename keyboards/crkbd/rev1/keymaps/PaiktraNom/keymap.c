3d1a008bfc5b883eefc85826624cc9d3b4a31f3b
PaiktraNom <55154237+PaiktraNom@users.noreply.github.com>
2023-07-26 20:45:36 -0400

    Modified some keys and added shift+back

--- keyboards/crkbd/rev1/keymaps/PaiktraNom/keymap.c
+++ keyboards/crkbd/rev1/keymaps/PaiktraNom/keymap.c
@@ -50,6 +50,7 @@ user_config_t user_config;
 //Layer toggle and switch
 #define T_SYM TT(_SYM)
 #define S_SYM MO(_SYM)
+#define T_UTIL TT(_UTIL)
 #define S_ARROWNUM MO(_ARROWNUM)
 #define T_GAMING TG(_GAMING)
 #define S_EXT MO(_GAMING_EXT)
@@ -69,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_BACKSLASH,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_LALT,S_ARROWNUM,SFT_SPC,   SFT_ENT,    S_SYM, KC_LGUI
+                                         KC_LALT,S_ARROWNUM,SFT_SPC,   SFT_ENT,    S_SYM, KC_RGUI
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -90,23 +91,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_GRV,    KC_1,    KC_2, _______,    KC_3,    KC_4,                      _______, _______, _______, _______, _______,  KC_DEL,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LBRC, KC_RBRC, _______, _______, _______,    KC_5,                      _______, _______, _______, _______, _______, _______,\
+      KC_LSFT, KC_RBRC, _______, _______, _______,    KC_5,                      _______, _______, _______, _______, _______, _______,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_BSLS,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      _______, _______, _______, _______, _______, _______,\
+      KC_LCTL,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      _______, _______, _______, _______, _______, _______,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,     KC_ENT, _______, _______ \
+                                          _______, _______, _______,     KC_ENT,  T_UTIL, _______ \
                                       //`--------------------------'  `--------------------------'
   ),
 
     [_ARROWNUM] = LAYOUT_split_3x6_3(
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, KC_GRV, KC_PAUS,   KC_UP,  KC_DEL, KC_PGUP,                         KC_1,    KC_2,    KC_3,    KC_0,KC_KP_ASTERISK, KC_BSPC,
+       KC_TAB,  KC_GRV, KC_PAUS,   KC_UP,  KC_DEL, KC_PGUP,                         KC_1,    KC_2,    KC_3,    KC_0,KC_KP_ASTERISK, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL, XXXXXXX, KC_LEFT, KC_DOWN,KC_RIGHT, KC_PGDN,                         KC_4,    KC_5,    KC_6,KC_KP_PLUS,KC_KP_SLASH, KC_KP_EQUAL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,  KC_END, KC_HOME,                         KC_7,    KC_8,    KC_9,KC_KP_MINUS, KC_KP_DOT, XXXXXXX,
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,  KC_END, KC_HOME,                         KC_7,    KC_8,    KC_9,KC_KP_MINUS, KC_KP_DOT, KC_KP_COMMA,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______, SFT_SPC,    SFT_ENT,T_GAMING,    KC_0
+                                          KC_LGUI, _______, SFT_SPC,    SFT_ENT,  T_UTIL,    KC_0
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -118,19 +119,63 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT,T_GAMING, SFT_SPC,    SFT_ENT, _______, KC_RALT
+                                          KC_LALT,  T_UTIL, SFT_SPC,    SFT_ENT, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
       [_UTIL] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  QWERTY,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX,  GAMING,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______, SFT_SPC,    SFT_ENT, _______, KC_RALT
+                                          _______, _______, SFT_SPC,    SFT_ENT, _______, _______,
                                       //`--------------------------'  `--------------------------'
   )
+};
+
+
+// Initialize variable holding the binary
+// representation of active modifiers.
+uint8_t mod_state;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // Store the current modifier state in the variable for later reference
+    mod_state = get_mods();
+    switch (keycode) {
+
+    case KC_BSPC:
+        {
+        // Initialize a boolean variable that keeps track
+        // of the delete key status: registered or not?
+        static bool delkey_registered;
+        if (record->event.pressed) {
+            // Detect the activation of either shift keys
+            if (mod_state & MOD_MASK_SHIFT) {
+                // First temporarily canceling both shifts so that
+                // shift isn't applied to the KC_DEL keycode
+                del_mods(MOD_MASK_SHIFT);
+                register_code(KC_DEL);
+                // Update the boolean variable to reflect the status of KC_DEL
+                delkey_registered = true;
+                // Reapplying modifier state so that the held shift key(s)
+                // still work even after having tapped the Backspace/Delete key.
+                set_mods(mod_state);
+                return false;
+            }
+        } else { // on release of KC_BSPC
+            // In case KC_DEL is still being sent even after the release of KC_BSPC
+            if (delkey_registered) {
+                unregister_code(KC_DEL);
+                delkey_registered = false;
+                return false;
+            }
+        }
+        // Let QMK process the KC_BSPC keycode as usual outside of shift
+        return true;
+    }
+
+    }
+    return true;
 };
